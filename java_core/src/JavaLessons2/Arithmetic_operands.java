686472d4582f373cff4cb19256e3de4ac1708792
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-03-12 23:24:20 +0500

    JavaCore

---
+++ java_core/src/JavaLessons2/Arithmetic_operands.java
@@ -0,0 +1,19 @@
+package JavaLessons2;
+
+public class Arithmetic_operands {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        int c = 7;
+        int d = 3;
+        int sum = a + b + c - d;
+        System.out.println("Sum " + sum);
+        int subtraction = b - a;
+        System.out.println("Subtr " + subtraction);
+
+        int mult = a * b;
+        int div = b / a;
+        System.out.println("Mult: " + mult);
+        System.out.println("Div: " + div);
+    }
+}
