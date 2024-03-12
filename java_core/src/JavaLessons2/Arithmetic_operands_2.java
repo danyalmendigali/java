873fd3412cdf1f7ea9d153ab5c9b003ddbe72324
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-03-12 23:35:24 +0500

    JavaCore

---
+++ java_core/src/JavaLessons2/Arithmetic_operands_2.java
@@ -0,0 +1,12 @@
+package JavaLessons2;
+
+public class Arithmetic_operands_2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int mod = a % b;
+        int mod2 = a / b;
+        System.out.println("Mod " + mod);
+        System.out.println("Div " + mod2);
+    }
+}
---
+++ java_core/src/JavaLessons2/Arithmetic_operands_3.java
@@ -0,0 +1,11 @@
+package JavaLessons2;
+
+public class Arithmetic_operands_3 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a & b); //
+        System.out.println(a | b);
+        System.out.println(a ^ b);
+    }
+}
