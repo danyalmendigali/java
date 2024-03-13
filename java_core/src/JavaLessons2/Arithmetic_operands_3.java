3859b891a9583411dc626989546677552926057a
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-03-13 23:19:56 +0500

    JavaCore

--- java_core/src/JavaLessons2/Arithmetic_operands_3.java
+++ java_core/src/JavaLessons2/Arithmetic_operands_3.java
@@ -4,8 +4,16 @@ public class Arithmetic_operands_3 {
     public static void main(String[] args) {
         boolean a = true;
         boolean b = false;
-        System.out.println(a & b); //
+        System.out.println(a & b);
         System.out.println(a | b);
         System.out.println(a ^ b);
+        int aa = 2;
+        int bb = 3;
+        // 10
+        // 11
+        // 01
+        System.out.println(aa ^ bb);
+
+
     }
 }
